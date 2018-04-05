f08683d1da712e6f006625879ff4785c84e13068
Machine <machine@machine-2.0>
2018-04-05 11:15:59 +0300

    ESlint fixed

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,14 @@
-const debug = require('debug')('weathermap');
+//  const debug = require('debug')('weathermap');
 
 const Koa = require('koa');
 const router = require('koa-router')();
 const fetch = require('node-fetch');
 const cors = require('kcors');
 
-const appId = process.env.APPID || '';
-const mapURI = process.env.MAP_ENDPOINT || "http://api.openweathermap.org/data/2.5";
-const targetCity = process.env.TARGET_CITY || "Helsinki,fi";
+const API_KEY = '91931bbedeaee3ede7a5eb46cbd6fe21';
+const appId = process.env.APPID || API_KEY;
+const mapURI = process.env.MAP_ENDPOINT || 'http://api.openweathermap.org/data/2.5';
+const targetCity = process.env.TARGET_CITY || 'Helsinki,fi';
 
 const port = process.env.PORT || 9000;
 
@@ -19,7 +20,7 @@ const fetchWeather = async () => {
   const endpoint = `${mapURI}/weather?q=${targetCity}&appid=${appId}&`;
   const response = await fetch(endpoint);
 
-  return response ? response.json() : {}
+  return response ? response.json() : {};
 };
 
 router.get('/api/weather', async ctx => {
