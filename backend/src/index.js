51982f9d1e03fffbd45ff9f4913d126ad9eec0f7
IO89 <osipov.ivan.89@gmail.com>
2019-05-02 16:33:00 +0300

    updated readme

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,12 @@
-const debug = require('debug')('weathermap');
-const Koa = require('koa');
-const router = require('koa-router')();
-const fetch = require('node-fetch');
-const cors = require('kcors');
+const Koa = require("koa");
+const router = require("koa-router")();
+const fetch = require("node-fetch");
+const cors = require("kcors");
 
-const API_KEY = '91931bbedeaee3ede7a5eb46cbd6fe21';
+const API_KEY = "Insert your key here";
 const appId = process.env.APPID || API_KEY;
-const mapURI = process.env.MAP_ENDPOINT || 'http://api.openweathermap.org/data/2.5';
+const mapURI =
+  process.env.MAP_ENDPOINT || "http://api.openweathermap.org/data/2.5";
 // const targetCity = process.env.TARGET_CITY || 'Helsinki,fi';
 const port = process.env.PORT || 9000;
 
@@ -23,13 +23,13 @@ const fetchWeatherLocal = async (lat, lon) => {
 // Route for get request with parameters from client browser(lat&lon)
 // Then calling fetchWeatherLocal with lat&lon
 // Sending back slice of array for today and next 3 and 6 hours
-router.get('/api/forecast/:coords', async ctx => {
+router.get("/api/forecast/:coords", async ctx => {
   let coordinates = ctx.params.coords;
-  coordinates = coordinates.split('&');
+  coordinates = coordinates.split("&");
   const lat = coordinates[0];
   const lon = coordinates[1];
   const weatherData = await fetchWeatherLocal(lat, lon);
-  ctx.type = 'application/json; charset=utf-8';
+  ctx.type = "application/json; charset=utf-8";
   ctx.body = weatherData.list.slice(0, 3) ? weatherData.list.slice(0, 3) : {};
 });
 
--- frontend/README.md
+++ frontend/README.md
@@ -1,8 +1,15 @@
 ### To launch front-end side of app execute following commands in terminal:
+
 ### Building a container:
- sudo docker build -t weatherapp_front .
+
+`sudo docker build -t weatherapp_front .`
+
 ### Run container on port 8000 and remove when exit
- sudo docker run --rm -i -p 8000:8000 --name weatherapp_front -t weatherapp_front
- ### Run container with volume
-sudo docker run --rm -p 8000:8000 -v $(pwd):/src/frontend --name weatherapp_front -t weatherapp_front
+
+`sudo docker run --rm -i -p 8000:8000 --name weatherapp_front -t weatherapp_front`
+
+### Run container with volume
+
+`sudo docker run --rm -p 8000:8000 -v $(pwd):/src/frontend --name weatherapp_front -t weatherapp_front`
+
 #### Client side receive lat and lon from a browser and then make a GET request to backend with parmaters, afterwards suppose to receive weather data for user location. Weather data is weather today and next 3 and 6 hours.
